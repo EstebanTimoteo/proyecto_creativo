e4edcbe00193a099db6e54bdd04c85b82c56e71f
Esteban Timoteo <nabirp_nabetse@hotmail.com>
2026-05-18 10:44:27 -0600

    agregado de html's principales al vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         // Esta es la entrada principal (index.html)
-        main: resolve(__dirname, "index.html"),
+        index: resolve(__dirname, "index.html"),
+        whoarewe: resolve(__dirname, 'whoarewe.html'),
+        aviso_legal: resolve(__dirname, 'aviso_legal.html'),
+        contacto: resolve(__dirname, 'contacto.html'),
+        cursos: resolve(__dirname, 'cursos.html'),
+        login: resolve(__dirname, 'login.html'),
+        noticias: resolve(__dirname, 'noticias.html'),
+        registro: resolve(__dirname, 'registro.html')
 
         /* 
            PARA AÑADIR MÁS PÁGINAS:
