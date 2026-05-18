3727dd01f995e49982170f91466b1dac36639e3a
Esteban Timoteo <nabirp_nabetse@hotmail.com>
2026-05-18 10:53:16 -0600

    agregado de html's dentro de carpetas en vite.config

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,24 @@ export default defineConfig({
       input: {
         // Esta es la entrada principal (index.html)
         index: resolve(__dirname, "index.html"),
-        whoarewe: resolve(__dirname, 'whoarewe.html'),
-        aviso_legal: resolve(__dirname, 'aviso_legal.html'),
-        contacto: resolve(__dirname, 'contacto.html'),
-        cursos: resolve(__dirname, 'cursos.html'),
-        login: resolve(__dirname, 'login.html'),
-        noticias: resolve(__dirname, 'noticias.html'),
-        registro: resolve(__dirname, 'registro.html')
+        whoarewe: resolve(__dirname, "whoarewe.html"),
+        aviso_legal: resolve(__dirname, "aviso_legal.html"),
+        contacto: resolve(__dirname, "contacto.html"),
+
+        cursos: resolve(__dirname, "cursos.html"),
+        cursos: resolve(__dirname, "cursos/ciberseguridad.html"),
+        cursos: resolve(__dirname, "cursos/desarrollo_blockchain.html"),
+        cursos: resolve(__dirname, "cursos/desarrollo_web.html"),
+        cursos: resolve(__dirname, "cursos/inteligencia_artificial.html"),
+
+        login: resolve(__dirname, "login.html"),
+
+        noticias: resolve(__dirname, "noticias.html"),
+        noticias: resolve(__dirname, "noticias/algoritmo.html"),
+        noticias: resolve(__dirname, "noticias/diagrama_flujo.html"),
+        noticias: resolve(__dirname, "noticias/lenguaje_natural.html"),
+
+        registro: resolve(__dirname, "registro.html"),
 
         /* 
            PARA AÑADIR MÁS PÁGINAS:
